7914f4a4533515acf8d5c227bcd22b5f28ecf749
Jimmy Chiu <jimmychiu11@gmail.com>
2026-02-20 21:22:51 +0800

    Add GitHub Pages deployment config

--- Spc.Web/vite.config.js
+++ Spc.Web/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/spc-simulator/',
   plugins: [vue()],
   server: {
     port: 5173,
